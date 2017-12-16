de32450b216001aa8180c28a45e1a32707f04c19
Luke Chambers <lukenatchambers@gmail.com>
2017-12-16 12:21:38 -0700

    Iteration one complete

--- lib/server.rb
+++ lib/server.rb
@@ -1,44 +1,14 @@
-require "socket"
-require "uri"
-require "pry"
-
-WEB_ROOT = "./lib"
-
-CONTENT_TYPE_MAPPING = {"html" => "text/html",
-                        "txt"  => "text/plain",
-                        "png"  => "image/png",
-                        "jpg"  => "image/jpeg"}
-
-DEFAULT_CONTENT_TYPE = "application/octet-stream"
-
-def content_type(path)
-  ext = File.extname(path).split(".").last
-  CONTENT_TYPE_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
-end
-
-def requested_file(request_line)
-  request_uri = request_line[1]
-  path        = URI.unescape(URI(request_uri).path)
-
-  clean = []
-
-  parts = path.split("/")
-
-  parts.each do |part|
-    next if part.empty? || part == "."
-    part == ".." ? clean.pop : clean << part
-  end
-
-  File.join(WEB_ROOT, *clean)
-end
+require 'socket'
 
 tcp_server = TCPServer.new(9292)
+
 count = 0
 
 loop do
   puts "Ready for a request"
   listener = tcp_server.accept
   count += 1
+  request_lines = []
   while line = listener.gets and !line.chomp.empty?
     request_lines << line.chomp
   end
@@ -46,29 +16,17 @@ loop do
   puts "Got this request:"
   puts request_lines.inspect
 
-  path = requested_file(request_lines)
-
-  if File.exist?(path) && !File.directory?(path)
-    File.open(path, "rb") do file
-      listener.print "HTTP/1.1 200 ok\r\n" +
-                     "content-type: #{content_type(file)}\r\n" +
-                     "content-length: #{file.size}\r\n"
-
-      IO.copy_stream(file, listener)
-    end
-  else
-    response = "File not found"
-
-    listener.print "HTTP/1.1 404 Not Found\r\n" +
-                   "content_type: text/plain\r\n" +
-                   "content_length: #{response.size}\r\n"
-
-    listener.print message
-  end
-
   puts "Sending response."
   response = "<pre>" + request_lines.join("\n") + "</pre>"
-  output = "<html><head></head><body>Hello World! (#{count})</body></html>"
+  output = "<html><head></head><body><pre>
+Verb: POST
+Path: /
+Protocol: HTTP/1.1
+Host: 127.0.0.1
+Port: 9292
+Origin: 127.0.0.1
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+</pre>Hello World! (#{count})</body></html>"
   headers = ["http/1.1 200 ok",
         "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
         "server: ruby",
