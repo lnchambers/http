44ddfa57565071199b086f8649d2e6b3667ccced
Luke Chambers <lukenatchambers@gmail.com>
2017-12-17 15:22:47 -0700

    Add test for edge case

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -88,4 +88,16 @@ class ParserTest < Minitest::Test
                       "Connection: keep-alive"])
   end
 
+  def test_params_does_not_accept_wrong_input
+    parser = Parser.new
+
+    assert_nil parser.params(["GET /word_search?param= HTTP/1.1",
+                      "Host: localhost:9292",
+                      "Upgrade-Insecure-Requests: 1",
+                      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                      "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7",
+                      "Accept-Language: en-us",
+                      "Accept-Encoding: gzip, deflate",
+                      "Connection: keep-alive"])
+  end
 end
